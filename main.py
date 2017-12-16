03adc123e54a8d5e8f2d73778245f621ba7b3180
Lijia Li <lil@oxy.edu>
2017-12-16 18:22:42 +0800

    update get verbs; clean code; add get tool

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import logging
 import gensim
 import time
 import spacy
+import nltk.stem
 
 # start timing
-start = time.time()
+tic = time.time()
+
+# download wordnet
+nltk.download('wordnet')
 
 # Login
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -14,86 +18,128 @@ model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vect
 # model = gensim.models.KeyedVectors.load_word2vec_format('./model/freebase-vectors-skipgram1000-en.bin', binary=True)
 
 
-# canons should be a list of pair of "noun verb"
-# this function take in a list of canonical word pair, and a noun to predict the top 10 related word
-def demo_more_verb (conons, n2):
-    m = 0
+# helper function that compute the average sigma (vector difference of word pair) of a list of given conons
+def get_ave_sigma(conons):
     sigma = 0
     for pair in conons:
-        v1, n1 = pair.split()
-        sigma += model.word_vec(v1) - model.word_vec(n1)
-        m += 1
-        predicted = model.most_similar([n2, v1], [n1])[0][0]
-        print('{} : {} :: [{}] : {}'.format(v1, n1, predicted, n2))
-    a = (1 / m) * sigma
-    predicted = model.most_similar([a, n2], [])
-    print("--------------------------------------------")
-    for i in range(10):
-        print('canon :: [{}] : {})'.format(predicted[i][0], n2))
+        v, n = pair.split()
+        sigma += model.word_vec(v) - model.word_vec(n)
+    ave_sigma = (1 / len(conons)) * sigma
+    return ave_sigma
 
 
+# return a list of lemmatized verbs that the noun can afford
+def get_verbs_for_noun(noun):
+    # prepare tools
+    wnl = nltk.stem.WordNetLemmatizer()
+    conons = list(filter(None, [line.rstrip() for line in open('./word_lists/verb_noun_pair.txt')]))
+    sigma = get_ave_sigma(conons)
 
-# TODO: Output predicted word, but not necessarily verb.
-# TODO: Also, it's hard to check they are verb or not if out of context.
-    # ??? isverb is a boolean to indicate whether to predict verb or not
-def predict_words (conons, word, word_position):
-    m = 0
-    sigma = 0
-    for pair in conons:
-        w1, w2 = pair.split()
-        sigma += model.word_vec(w1) - model.word_vec(w2)
-        m += 1
-    a = (1/m)*sigma
-    if word_position == 0:
-        predicted = model.most_similar([word], [a], topn = 10)
-        return predicted
-    elif word_position == 1:
-        predicted = model.most_similar([a, word], [], topn = 10)
-        return predicted
-    else:
-        print("please put in correct cannon position")
+    # list of common used verbs
+    navigation_verbs = ["north", "south", "east", "west", "northeast", "southeast", "southwest", "northwest", "up",
+                        "down", "enter", "exit"]
+    essential_manipulation_verbs = ["get", "drop", "push", "pull", "open", "close"]
+    verb_list = list(filter(None, [line.rstrip() for line in open('./word_lists/top_1000_verbs.txt')]))
+
+    # extract words from word2vec model & append lemmatized word to list
+    model_verb = model.most_similar([sigma, noun], [], topn=10)
+    word2vec_words = []
+    for verb in model_verb:
+        word2vec_words.append(wnl.lemmatize(str(verb[0].lower())))
+
+    # set operations
+    affordant_verbs = list(set(verb_list) & set(word2vec_words))
+    final_verbs = list(set(navigation_verbs) | set(essential_manipulation_verbs) | set(affordant_verbs))
+
+    # -----------test lines (uncomment below four lines to view different set of verbs)-------------
+    #     print("-"*10, noun, "-"*10)
+    #     print("word2vec words: ", word2vec_words)
+    #     print("affordant verbs: ", affordant_verbs)
+    #     print("final verbs: ", final_verbs)
+
+    return affordant_verbs
 
 
-# TODO check the graspability of the object (Fulda)
-# TODO the output is not yet guarentee verbs
-def pre_possible_action(sentence):
+# return a list of adjectives that describe the given noun
+def get_adjectives_for_noun(noun):
+    conons = list(filter(None, [line.rstrip() for line in open('./word_lists/noun_adj_pair.txt')]))
+    #     conons = ["knife sharp", "light bright", "ice cold", "fire burning", "desert dry", "sky blue", "night dark",
+    #                 "rope long"]
+    sigma = get_ave_sigma(conons)
+    model_adj = model.most_similar([sigma, noun], [], topn=10)
+    word2vec_adj = []
+    for adj in model_adj:
+        word2vec_adj.append(adj[0])
+    return word2vec_adj
+
+
+# return a list of possible actions by compute affordable actions on nouns in the given sentence
+def possible_actions(sentence):
+    # prepare tools
     nlp = spacy.load('en')
     doc = nlp(sentence)
-    canons = ["sing song", "drink water", "read book", "eat food", "wear coat", "drive car", "ride horse",
-              "give gift", "attack enemy", "say word", "open door", "climb tree", "heal wound", "cure disease",
-              "paint picture"]
+    wnl = nltk.stem.WordNetLemmatizer()
+    conons = list(filter(None, [line.rstrip() for line in open('./word_lists/verb_noun_pair.txt')]))
+
+    # create dictionary in the form [noun: verbs]
     dictionary = {}
     for chunk in doc.noun_chunks:
-        word = chunk.root.text
+        word = wnl.lemmatize(chunk.root.text)
         if word not in dictionary:
-            dictionary[word] = predict_words(canons, word, 1)
-    return dictionary
+            dictionary[word] = get_verbs_for_noun(word)
+
+    # loop through dictionary to creat action list
+    action_pair = []
+    for key, values in dictionary.items():
+        [action_pair.append(value + " " + key) for value in values]
+    return action_pair
 
 
-def pretty_print_dict(dictionary):
-    print("")
-    for key, val in dictionary.items():
-        print(key, ":", val)
+def get_tools_for_verb(verb):
+    conons = list(filter(None, [line.rstrip() for line in open('./word_lists/verb_noun_pair.txt')]))
+    sigma = get_ave_sigma(conons)
+
+    model_tools = model.most_similar([verb], [sigma], topn=10)
+    word2vec_tools = []
+    for tool in model_tools:
+        word2vec_tools.append(tool[0])
+    return word2vec_tools
 
 
 def main():
-    tic = time.time()
-    verb_noun = ["sing song", "drink water", "read book", "eat food", "wear coat", "drive car", "ride horse", "give gift",
-                 "attack enemy", "say word", "open door", "climb tree", "heal wound", "cure disease", "paint picture"]
-    noun_adj = ["knife sharp", "light bright", "ice cold", "fire burning", "desert dry", "sky blue", "night dark", "rope long"]
-    words = ["book", "sword", "horse", "key"]
-    for word in words:
-        print(predict_words(verb_noun, word, 1))
-
+    # prepare samples
+    test_nouns = ["book", "sword", "horse", "key"]
+    test_verbs = ["climb", "use", "open", "lift", "kill", "murder", "drive", "ride", "cure", "type", "sing"]
     s = "Soon you’ll be able to send and receive money from friends and family right in Messages."
     s1 = "This is an open field west of a white house, with a boarded front door. There is a small mailbox here."
     s2 = "This is a forest, with trees in all directions around you."
     s3 = "This is a dimly lit forest, with large trees all around.  One particularly large tree with some low branches stands here."
     sentences = [s, s1, s2, s3]
+
+
+    # run samples
+
+    # get_verbs_for_noun tests
+    print("-" * 5, "get_verbs_for_noun function tests", "-" * 5)
+    [print(noun, ":", get_verbs_for_noun(noun)) for noun in test_nouns]
+    print()
+
+    # get_adjectives_for_noun tests
+    print("-" * 5, "get_adjectives_for_noun function tests", "-" * 5)
+    [print(noun, ":", get_adjectives_for_noun(noun)) for noun in test_nouns]
+
+    # possible_actions tests
     for sentence in sentences:
-        print("-" * 3, sentence, "-" * 3)
-        pretty_print_dict(pre_possible_action(sentence))
+        print()
+        print(sentence)
+        print(possible_actions(sentence))
+
+    # get_tools_for_verb tests
+    [print(verb, ":", get_tools_for_verb(verb)) for verb in test_verbs]
+
     toc = time.time()
-    # print("total time spend:", toc - tic, "s")
+    print("total time spend:", toc - tic, "s")
+
+    # if __name__ == "__main__": main()
 
 if __name__ == "__main__": main()
